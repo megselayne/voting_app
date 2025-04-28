7b04d1dd29945716c646c3fba5f1cb587ba68d7c
Megan Lee <meganlee@Megans-MacBook-Pro-2.local>
2025-04-27 22:15:59 -0600

    feat: user email displays on sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user_email
+
+    def current_user_email
+      @current_user_email ||= User.find_by(id: session[:user_id])&.email
+    end
 end
